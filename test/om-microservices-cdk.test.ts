99f1407e04538dae93b9798363717351cc804087
Prince Andrew Anire <andrewcustomerhelper@gmail.com>
2024-11-18 05:41:14 +0000

    Initial commit

---
+++ test/om-microservices-cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as OmMicroservicesCdk from '../lib/om-microservices-cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/om-microservices-cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new OmMicroservicesCdk.OmMicroservicesCdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
